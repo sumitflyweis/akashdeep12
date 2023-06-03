15683792a5d66e1fa432760a580a50aee9d6a4ae
sumitflyweis <node3@flyweis.technology>
2023-06-03 16:03:14 +0530

    d

--- src/controller/remittance.js
+++ src/controller/remittance.js
@@ -1,110 +1,111 @@
 const Remittance = require('../model/remittance'); // Replace the path to the remittance model with your actual file path
+const axios = require("axios")
 
 
 
 
-exports.createRemittance = async (req, res) => {
-  try {
-    const {
-      beneficiary_id,
-      account_holder_name,
-      account_number,
-      swift_code,
-      iban,
-      routing_number,
-      bank_name,
-      bank_country,
-      bank_address,
-      sort_code,
-      transit_code,
-      bsb_number,
-      address,
-      city,
-      state,
-      country,
-      postal_code
-    } = req.body;
+// exports.createRemittance = async (req, res) => {
+//   try {
+//     const {
+//       beneficiary_id,
+//       account_holder_name,
+//       account_number,
+//       swift_code,
+//       iban,
+//       routing_number,
+//       bank_name,
+//       bank_country,
+//       bank_address,
+//       sort_code,
+//       transit_code,
+//       bsb_number,
+//       address,
+//       city,
+//       state,
+//       country,
+//       postal_code
+//     } = req.body;
 
-    const newBeneficiary = new Beneficiary({
-      beneficiary_id,
-      account_holder_name,
-      account_number,
-      swift_code,
-      iban,
-      routing_number,
-      bank_name,
-      bank_country,
-      bank_address,
-      sort_code,
-      transit_code,
-      bsb_number,
-      address,
-      city,
-      state,
-      country,
-      postal_code
-    });
+//     const newBeneficiary = new Remittance({
+//       beneficiary_id,
+//       account_holder_name,
+//       account_number,
+//       swift_code,
+//       iban,
+//       routing_number,
+//       bank_name,
+//       bank_country,
+//       bank_address,
+//       sort_code,
+//       transit_code,
+//       bsb_number,
+//       address,
+//       city,
+//       state,
+//       country,
+//       postal_code
+//     });
 
   
     
-        const clientId = "TEST370281a1d99b47aa3a41930df0182073"
-        const clientSecret = "TEST95fd8451c7e275d78ddb4c769b20c92bdd1f3448"
+//         const clientId = "TEST370281a1d99b47aa3a41930df0182073"
+//         const clientSecret = "TEST95fd8451c7e275d78ddb4c769b20c92bdd1f3448"
     
-        const headers = {
-          "x-api-version":"2023-03-01",
-          "Content-Type": "application/json",
-          "X-Client-ID": clientId,
-          "X-Client-Secret": clientSecret,
-        };
+//         const headers = {
+//           "x-api-version":"2023-03-01",
+//           "Content-Type": "application/json",
+//           "X-Client-ID": clientId,
+//           "X-Client-Secret": clientSecret,
+//         };
     
     
-        console.log(headers)
-    const response = await axios.post("https://sandbox.cashfree.com/pg/lrs/remitters", newBeneficiary,{
-      headers: headers 
-    });
-
-    
-    console.log(response)
-    // Extract the created beneficiary from the response
-    const createdBeneficiary = response.data;
-
-    res.status(201).json(createdBeneficiary);
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
-
-
-
-
-
-
-// // Create a new remittance
-// exports.createRemittance = async (req, res) => {
-//   try {
-//     // Get data from request body
-//     const { city, optionBestDescribeYou, name, mobile, email, monthlyImport_Export } = req.body;
-
-//     // Create a new remittance document
-//     const remittance = new Remittance({
-//       city,
-//       optionBestDescribeYou,
-//       name,
-//       mobile,
-//       email,
-//       monthlyImport_Export
+//         console.log(headers)
+//     const response = await axios.post("https://sandbox.cashfree.com/pg/lrs/remitters", newBeneficiary,{
+//       headers: headers 
 //     });
 
-//     // Save the remittance document to the database
-//     const result = await remittance.save();
-//     res.status(201).json({ message: 'Remittance created successfully', data: result });
+    
+//     console.log(response)
+//     // Extract the created beneficiary from the response
+//     const createdBeneficiary = response.data;
+
+//     res.status(201).json(createdBeneficiary);
 //   } catch (error) {
-//     console.error(error); // Log the error for debugging purposes
-//     res.status(500).json({ message: 'Internal server error' });
+//     console.log(error)
+//     res.status(500).json({ error: "Internal server error" });
 //   }
 // };
+
+
+
+
+
+
+
+// Create a new remittance
+exports.createRemittance = async (req, res) => {
+  try {
+    // Get data from request body
+    const { city, optionBestDescribeYou, name, mobile, email, monthlyImport_Export } = req.body;
+
+    // Create a new remittance document
+    const remittance = new Remittance({
+      city,
+      optionBestDescribeYou,
+      name,
+      mobile,
+      email,
+      monthlyImport_Export
+    });
+
+    // Save the remittance document to the database
+    const result = await remittance.save();
+    res.status(201).json({ message: 'Remittance created successfully', data: result });
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 
 
